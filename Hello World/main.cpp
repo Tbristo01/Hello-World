63055ce5d90b3a646b754d32409f85da564fc351
Tishaun Bristol <tishaunbristol@me.com>
2019-01-29 18:13:19 -0500

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Tishaun Bristol on 1/29/19.
+//  Copyright © 2019 Bristol Developers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
